96cb070b2d9ef7e4c94cbd5a8c2750acebb846ca
JamesTribe <jjtribe93@gmail.com>
2023-06-15 14:40:04 +0100

    trying to use import statements

--- assets/haikus.js
+++ assets/haikus.js
@@ -3,3 +3,34 @@ const haikus = [
     "haikutwo",
     "haikuthree"
 ]
+
+const haikuObject = [
+  {
+    title: "Autumn",
+    body: "Autumn is great,\nSitting and napping,\nAll day long",
+    author: "James Tribe"
+  },
+  {
+    title: "Whispering Wind",
+    body: "Gentle breeze whispers,\nCarrying secrets of life,\nNature's lullaby",
+    author: "Sakura Nakamura"
+  },
+  {
+    title: "Moonlit Night",
+    body: "Moon shines in the night sky,\nGuiding wanderers below,\nSilver luminescence",
+    author: "Ethan Johnson"
+  },
+  {
+    title: "Cherry Blossoms",
+    body: "Pink petals dancing,\nFragrant embrace fills the air,\nSpring's fleeting beauty",
+    author: "Aria Lee"
+  },
+  {
+    title: "Mountain Serenity",
+    body: "Peaceful mountain view,\nSilent majesty unfolds,\nNature's sanctuary",
+    author: "Kai Chen"
+  }
+];
+
+export default haikus.js;
+
--- main.js
+++ main.js
@@ -1,8 +1,39 @@
+//   import haikuObject from "./assets/haikus.js";
+// When I try to import this, I get the error 'cannot use import statement outside a module'. 
+
 const haikus = [
     "haikuone",
     "haikutwo",
     "haikuthree"
 ]
+
+const haikuObject = [
+    {
+        title: "Autumn",
+        body: "Autumn is great,\nSitting and napping,\nAll day long",
+        author: "James Tribe"
+    },
+    {
+        title: "Whispering Wind",
+        body: "Gentle breeze whispers,\nCarrying secrets of life,\nNature's lullaby",
+        author: "Sakura Nakamura"
+    },
+    {
+        title: "Moonlit Night",
+        body: "Moon shines in the night sky,\nGuiding wanderers below,\nSilver luminescence",
+        author: "Ethan Johnson"
+    },
+    {
+        title: "Cherry Blossoms",
+        body: "Pink petals dancing,\nFragrant embrace fills the air,\nSpring's fleeting beauty",
+        author: "Aria Lee"
+    },
+    {
+        title: "Mountain Serenity",
+        body: "Peaceful mountain view,\nSilent majesty unfolds,\nNature's sanctuary",
+        author: "Kai Chen"
+    }
+];
 
 const app = Vue.createApp({
     data() {
@@ -19,9 +50,19 @@ const app = Vue.createApp({
             this.searchInput = event.target.value;
             console.log(this.searchInput)
         },
-        dealWithClick(){
-            const ranNumber = Math.floor(Math.random()*3)
+        dealWithClick() {
+            const ranNumber = Math.floor(Math.random() * 3)
             this.ourHaiku = haikus[ranNumber];
+        },
+        dealWithSearchClick() {
+            ourSearch = this.searchInput.toLowerCase();
+            for (let i = 0; i < haikuObject.length; i++) {
+                lowerCaseHaiku = haikuObject[i].body.toLowerCase()
+                if (lowerCaseHaiku.includes(ourSearch)) {
+                    this.ourHaiku = haikuObject[i].body
+                    break;
+                }
+            }
         }
     },
 })
