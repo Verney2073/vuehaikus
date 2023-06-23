3c560c582b015672412ddd2f4bad567ff8535201
JamesTribe <jjtribe93@gmail.com>
2023-06-23 11:52:47 +0100

    interval border on confucius image

--- main.js
+++ main.js
@@ -6,20 +6,42 @@ const app = Vue.createApp({
         return {
             test: "Haiku",
             confucius: "./assets/images/confuciusnightbar.png",
+            confuciusBorder: ["red", "blue", "pink", "yellow"],
+            confuciusBorderIndex: 0,
             mainHaiku: "",
-            searchInput: ""
+            searchInput: "",
+            localTime: ""
         }
     },
     methods: {
         dealWithInput(event) {
-                this.searchInput = event.target.value;
-                console.log(this.searchInput)
+            this.searchInput = event.target.value;
+            console.log(this.searchInput)
         },
         dealWithRandomClick(payload) {
             this.mainHaiku = payload.randomHaiku;
         },
         dealWithSearchClick(payload) {
             this.mainHaiku = payload.haikuBody;
+        },
+        controlImageInterval() {
+            var time = this;
+            setInterval(() => {
+                //this cool logic suggested by chatGPT, gives the remainder of an integer division... 
+                //e.g. if the index is 0, it'll be (0+1) /4 gives remainder 1. Next time it'll be 1+1 / 4 gives remainder 2... but 3+1 / 4 gives remainder 0
+                this.confuciusBorderIndex = (this.confuciusBorderIndex + 1) % this.confuciusBorder.length;
+            }, 3000);
         }
     },
+    computed: {
+        imageStyle() {
+            return {
+                border: `3px solid ${this.confuciusBorder[this.confuciusBorderIndex]}`,
+                padding: '15px',
+            };
+        },
+    },
+    mounted() {
+        this.controlImageInterval()
+    }
 })
